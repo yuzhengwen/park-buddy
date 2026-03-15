b787f5b67a83b3a56c9235250665bf5328d0df34
Yu Zhengwen <yuzhengwen0217@gmail.com>
2026-03-15 17:15:36 +0800

    Added supabase library and anon login page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
+import 'screens/login_screen.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Supabase.initialize(
+    url: 'https://saslrisbvxdwowextgbh.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNhc2xyaXNidnhkd293ZXh0Z2JoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM1NTcyMjcsImV4cCI6MjA4OTEzMzIyN30.QSNyqc0leDJ6IOnWQW053r4NFaq2nrnS5LCYPFaJ4s8',
+  );
   runApp(const MyApp());
 }
 
@@ -30,7 +38,8 @@ class MyApp extends StatelessWidget {
         // tested with just a hot reload.
         colorScheme: .fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      //home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: LoginScreen(), // start with login screen
     );
   }
 }
