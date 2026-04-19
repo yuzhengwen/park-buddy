eca3c3eb0d3662b2976f528935c56e8cb9cbb798
Brian Su <briansujl@gmail.com>
2026-04-19 11:10:16 +0800

    Fix extra app bar in my parking tab

--- lib/tabs/my_parking_tab.dart
+++ lib/tabs/my_parking_tab.dart
@@ -11,65 +11,63 @@ class MyParkingTab extends StatelessWidget {
   Widget build(BuildContext context) {
     final provider = context.watch<CarsProvider>();
 
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('My Parking', style: TextStyle(color: Colors.white)),
-        backgroundColor: Color(0xFFFF7643),
-      ),
-      body: provider.isLoading
-          ? Center(child: CircularProgressIndicator())
-          : RefreshIndicator(
-              onRefresh: context.read<CarsProvider>().loadCars,
-              child: provider.cars.isEmpty
-                  ? ListView(
-                      children: [
-                        SizedBox(height: MediaQuery.of(context).size.height * 0.2),
-                        Center(
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Icon(
-                                Icons.directions_car_filled_outlined,
-                                size: 100,
-                                color: Colors.grey.shade300,
+    return provider.isLoading
+        ? Center(child: CircularProgressIndicator())
+        : RefreshIndicator(
+            onRefresh: context.read<CarsProvider>().loadCars,
+            child: provider.cars.isEmpty
+                ? ListView(
+                    children: [
+                      SizedBox(
+                        height: MediaQuery.of(context).size.height * 0.2,
+                      ),
+                      Center(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(
+                              Icons.directions_car_filled_outlined,
+                              size: 100,
+                              color: Colors.grey.shade300,
+                            ),
+                            const SizedBox(height: 20),
+                            Text(
+                              'No cars added yet',
+                              style: TextStyle(
+                                fontSize: 22,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.grey.shade700,
                               ),
-                              const SizedBox(height: 20),
-                              Text(
-                                'No cars added yet',
+                            ),
+                            const SizedBox(height: 10),
+                            Padding(
+                              padding: const EdgeInsets.symmetric(
+                                horizontal: 40,
+                              ),
+                              child: Text(
+                                'Add your first car in your profile to get started!',
+                                textAlign: TextAlign.center,
                                 style: TextStyle(
-                                  fontSize: 22,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.grey.shade700,
+                                  fontSize: 16,
+                                  height: 1.5,
+                                  color: Colors.grey.shade500,
                                 ),
                               ),
-                              const SizedBox(height: 10),
-                              Padding(
-                                padding: const EdgeInsets.symmetric(horizontal: 40),
-                                child: Text(
-                                  'Add your first car in your profile to get started!',
-                                  textAlign: TextAlign.center,
-                                  style: TextStyle(
-                                    fontSize: 16,
-                                    height: 1.5,
-                                    color: Colors.grey.shade500,
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
+                            ),
+                          ],
                         ),
-                      ],
-                    )
-                  : ListView.builder(
-                      itemCount: provider.cars.length,
-                      itemBuilder: (context, index) {
-                        return CarCard(
-                          car: provider.cars[index],
-                          parkingService: ParkingService(),
-                        );
-                      },
-                    ),
-            ),
-    );
+                      ),
+                    ],
+                  )
+                : ListView.builder(
+                    itemCount: provider.cars.length,
+                    itemBuilder: (context, index) {
+                      return CarCard(
+                        car: provider.cars[index],
+                        parkingService: ParkingService(),
+                      );
+                    },
+                  ),
+          );
   }
 }
